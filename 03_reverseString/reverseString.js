d664c38fad05a22daf7571bf77efdd584f00c0e4
Davis <davismickens98@gmail.com>
2024-01-23 20:02:50 -0500

    Completed the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+function reverseString(string) {
+    let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
+}
 
-};
+/* without using built-in functions 
+function reverseString(string){
+    let newString = "";
+    for (i = string.length -1; i >=0; i--){
+        newString += string[i];
+    }
+    return newString
+}*/
+
 
 // Do not edit below this line
 module.exports = reverseString;
