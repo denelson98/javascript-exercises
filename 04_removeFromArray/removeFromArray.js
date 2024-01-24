370add234af8c97ead2263c91a46e30c63679990
Davis <davismickens98@gmail.com>
2024-01-24 18:42:37 -0500

    Completed exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-function removeFromArray(array, ...otherArguments) {
-    let cutArray = array.filter(item => item !== otherArguments); //try to pass firtst test first
+/*function removeFromArray(array, arg1, arg2, arg3, arg4) {
+    let cutArray = array.filter(item => item !==arg1 && item!==arg2 && item!==arg3 && item!= arg4); //try to pass firtst test first
     return cutArray;
+};*/
+
+//cleaner solution that actually can do as many args 
+function removeFromArray(array, ...args) {
+    return array.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
