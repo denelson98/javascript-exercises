0310e115c8b09c39f57728cb35ba3aa96836d411
Davis <davismickens98@gmail.com>
2024-01-23 19:33:26 -0500

    Completed exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+function repeatString(string, num) {
+    if (num >= 0){
+        let newString = string.repeat(num);
+        return newString;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
