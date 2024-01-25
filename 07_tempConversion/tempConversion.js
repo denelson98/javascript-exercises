e9cab2c8f7d5f5dcd744e3ef03abc09e1a261bd8
Davis <davismickens98@gmail.com>
2024-01-24 20:11:57 -0500

    Completed exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function convertToCelsius(fahrenheit) {
+  let temp = (fahrenheit - 32) * 5/9;
+  let roundedTemp = Math.round(temp*10)/10;
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+function convertToFahrenheit(celcius) {
+  let temp = (celcius * 9/5) + 32;
+  let roundedTemp = Math.round(temp*10)/10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
