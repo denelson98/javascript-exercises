dd22e2a681b4f51646a07cd18a1710880ca368d5
Davis <davismickens98@gmail.com>
2024-01-24 19:54:21 -0500

    Completed exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+function leapYears(year) {
+    if (year % 100 ==0 && year % 400 !==0) {
+        return false;
+    } else if (year % 4 !==0) {
+        return false;
+    } else
+    return true;
 };
 
 // Do not edit below this line
