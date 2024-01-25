728fbac4bb3d849b84d053f032e2b195a246b11d
Davis <davismickens98@gmail.com>
2024-01-24 19:35:42 -0500

    Completed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+function sumAll(min, max) {
+    if (min < 0 || max <0){
+        return "ERROR";
+    } else if (min > max && Number.isInteger(min) && Number.isInteger(max)) {
+        return min*(min+1)/2;
+    } else if (min < max && Number.isInteger(min) && Number.isInteger(max)){
+        return max*(max+1)/2;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
