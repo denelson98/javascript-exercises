988aad7ee72e478c72c1a042732d5d20fde09dbd
Davis <davismickens98@gmail.com>
2024-01-24 19:55:57 -0500

    Left comment for self

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -7,5 +7,11 @@ function leapYears(year) {
     return true;
 };
 
+/* cleaner solution
+function leapYears(year) {
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+}; */
+
+
 // Do not edit below this line
 module.exports = leapYears;
