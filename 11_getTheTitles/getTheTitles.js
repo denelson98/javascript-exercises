cee011a74ea7abf1b882983751413f18150a714f
Davis <davismickens98@gmail.com>
2024-02-15 21:03:36 -0500

    Completed exercise and added cleaner solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    /*let title1 = books[0].title
+    let title2 = books[1].title
+    let array = [title1, title2]
+    return array*/
+    return array.map((book) => book.title);
 };
 
 // Do not edit below this line
