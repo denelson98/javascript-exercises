dae34e6b4d30bf609a41b2164559d25bc447028f
Davis <davismickens98@gmail.com>
2024-02-15 19:54:54 -0500

    Added functions for all but factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,26 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let add = num1+num2
+  return add
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let subtract = num1 - num2
+  return subtract
 };
 
-const sum = function() {
-	
+const sum = function(...nums) {
+  let sum = 0;
+  for (let num of nums) sum += num;
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(num1, num2) {
+  let multiply = num1*num2
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let power = num1^num2
+  return power
 };
 
 const factorial = function() {
