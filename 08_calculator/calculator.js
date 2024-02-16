cc45c3a32e3d32bf8064a4504bdd48dc71e5df2b
Davis <davismickens98@gmail.com>
2024-02-15 20:39:03 -0500

    Completed exercise and added cleaner solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,30 +1,49 @@
 const add = function(num1, num2) {
-	let add = num1+num2
-  return add
+	/*let add = num1+num2
+  return add*/
+  return num1 + num2
 };
 
 const subtract = function(num1, num2) {
-	let subtract = num1 - num2
-  return subtract
+	/*let subtract = num1 - num2
+  return subtract*/
+  return num1 - num2
 };
 
-const sum = function(...nums) {
-  let sum = 0;
-  for (let num of nums) sum += num;
-  return sum
+const sum = function(array) {
+  /*let sum = 0
+  for (i=0; i < array.length; i++){
+    sum += array[i]
+  }
+  return sum*/
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function(num1, num2) {
-  let multiply = num1*num2
+const multiply = function(array) {
+  /*let multiply = 1
+  for (i=0; i < array.length; i++){
+    multiply *= array[i]
+  }
+  return multiply*/
+  return array.reduce((product, current) => product * current)
 };
 
 const power = function(num1, num2) {
-	let power = num1^num2
-  return power
+	/*let power = num1**num2
+  return power*/
+  return num1**num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = num
+  if (factorial == 0){
+    return 1;
+  } else {
+    for (i=1; i<num; i++){
+      factorial *= (num-i)
+    }
+  }
+  return factorial
 };
 
 // Do not edit below this line
