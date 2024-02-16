dc3efdcd2a6a0adaca9dc5db001dd311f38b5c7e
Davis <davismickens98@gmail.com>
2024-02-15 20:57:24 -0500

    Completed exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    let fib = [];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (i = 2; i < 30; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+      }
+    return fib[index]
 };
 
 // Do not edit below this line
